1721455ecf30d92e5ee7a94debd3f1dcd991a891
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-04-16 12:51:26 -0400

    added getting description

--- Houzz.py
+++ Houzz.py
@@ -13,8 +13,9 @@ class Houzz:
     for a single metadata file.
     """
     def __init__(self, base_folder, category):
-        # Assume Unix-style pathnames
-        # Remove trailing '/' from 'base_folder,' if present, to regularize input
+        """ Assume Unix-style pathnames
+        Remove trailing '/' from 'base_folder,' if present, to regularize input
+        """
         base_folder = base_folder.rstrip('/')
 
         # Set instance variables
@@ -24,25 +25,39 @@ class Houzz:
         self.data_folder = base_folder + '/metadata/' + category
 
     def loadmat(self, filename):
-        """An example of the Matlab format of the Houzz dataset
+        """An example of the Matlab format of the Houzz dataset (data_02101038)
         data =
-               url: [1x113 char]
-                id: 4945307
-        image_link: [1x75 char]
-             style: 'mediterranean'
-              type: 'bedroom'
-               tag: {''Bedroom-en-suite-%2F-Dar-Farah'}
-          sameproj: {1x15 cell}
-         recommend: {1x6 cell}
+            url: [1x83 char]
+             id: 2101038
+     image_link: [1x74 char]
+    description: [1x134 char]
+          style: 'contemporary'
+           type: 'bedroom'
+            tag: {'built-in-desk'  'guest-room-retreat'  'vaulted-ceilings'}
+       sameproj: {[1x77 char]  [1x83 char]  [1x87 char]}
+      recommend: {1x6 cell}
         """
-        py_mat = scipy.io.loadmat(filename)
+        py_mat = scipy.io.loadmat(filename, squeeze_me=True,
+                                  struct_as_record=False)
+        py_data = py_mat.data
+        attributes = dir(py_data)
+
         data = dict()
-        data['url'] = py_mat['data']['url'][0][0][0]
-        data['id'] = py_mat['data']['id'][0][0][0][0]
-        data['image_link'] = py_mat['data']['image_link'][0][0][0]
-        data['style'] = py_mat['data']['style'][0][0][0]
-        data['type'] = py_mat['data']['type'][0][0][0]
-        data['tag'] = py_mat['data']['tag'][0][0][0][0][0]
-        data['sameproj'] = py_mat['data']['sameproj'][0][0][0]  # 1 x 15 array
-        data['recommend'] = py_mat['data']['recommend'][0][0][0]  # 1 x 6 array
+
+        # for string array in matlab
+        data['url'] = py_data.url if 'url' in attributes else None
+        data['id'] = py_data.id if 'id' in attributes else None
+        data['image_link'] = py_data.image_link \
+            if 'image_link' in attributes else None
+        data['description'] = py_data.description \
+            if 'description' in attributes else None
+        data['style'] = py_data.style if 'style' in attributes else None
+        data['type'] = py_data.type if 'type' in attributes else None
+
+        # for cell array in Matlab
+        data['tag'] = [x for x in py_data.tag] if 'tag' in attributes else None
+        data['sameproj'] = [x for x in py_data.sameproj] \
+            if 'sameproj' in attributes else None
+        data['recommend'] = [x for x in py_data.recommend] \
+            if 'recommend' in attributes else None
         return data
