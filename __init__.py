19be843f58930b9474174cbaac351a0458cef950
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-04-19 17:13:25 -0400

    added textual feature computation function

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
 import scipy.io
-import numpy
 
 import gensim
-from nltk.corpus import stopwords
 
 from format_print import format_print
 
@@ -11,14 +9,14 @@ from format_print import format_print
 DATASET_ROOT = '/home/brian/LanguageVision/final_project/dataset/'
 
 # List of labels in our closed world
-LABELS = ["traditional", 
-          "contemporary", 
+LABELS = ["traditional",
+          "contemporary",
           "eclectic",
           "modern",
           "mediterranean",
           "tropical",
-          "asian"
-         ]
+          "asian"]
+
 
 class Houzz:
     """A class for processing the Houzz.com dataset:
@@ -36,7 +34,7 @@ class Houzz:
     # Define class constants
     N_TEXT_FEATURE = 300
 
-    def __init__(self, base_folder=DATASET_ROOT, category='bedroom', 
+    def __init__(self, base_folder=DATASET_ROOT, category='bedroom',
                  text_model_file='GoogleNews-vectors-negative300.bin'):
         """ Assume Unix-style pathnames
         Remove trailing '/' from 'base_folder,' if present, to regularize input
@@ -99,69 +97,6 @@ class Houzz:
         data['recommend'] = [x for x in py_data.recommend] if 'recommend' in attributes else None
         return data
 
-    def compute_text_feature(self, filename):
-        """Compute a mean vector to represent the words in description and tags
-
-        @param metadata
-            The metadata from self.loadmat
-        """
-        metadata = self.loadmat(filename)
-
-        if not metadata['tag'] and not metadata['description']:
-            format_print('No textual feature found')
-            return None
-
-        if metadata['tag']:
-            tag_feature = self.average_text_vector(metadata['tag'])
-
-        if metadata['description']:
-            des_feature = self.average_text_vector(
-                metadata['description'].split())
-
-        return tag_feature, des_feature
-
-    def process_text(self, text):
-        """
-        Each tag in the dataset is a phrase joined by hyphens. For example,
-
-            guest-room-retreat
-
-        This will not appear in the word2vec lexicon. Therefore,
-        split each tag into its constituent words and remove stop words.
-        Without a special corpus of interior design terms to train the
-        language model on, this seems like the best we can do.
-
-        @param text
-            a (possibly hyphenated) tag
-        @returns
-            a list of processed strings with hyphens and stop words removed
-        """
-        stop_list = stopwords.words('english')
-        omit = set(stop_list).union(set(LABELS))  # words we don't want to use
-
-        return [word for word in text.split('-') if word not in omit]
-
-    def average_text_vector(self, words):
-        """
-        Compute a feature vector for one term. If the term is a phrase joined
-        by hypehns, the function split it to words and average the individual
-        words as one vector.
-
-        @param words
-            a list of (possibly hyphenated) words
-        @returns
-            a normalized vector
-        """
-        feature = numpy.array([0] * self.N_TEXT_FEATURE, dtype=numpy.float32)
-        for word in words:
-            processed_words = self.process_text(word)
-            for processed_word in processed_words:
-                if processed_word in self.text_model:
-                    feature += self.text_model[processed_word]
-        # return a normalized feature
-        norm = numpy.linalg.norm(feature)
-        return feature / norm if norm else feature
-
     def check_path(self, filename):
         """
         Check the filename, prepend or append information if needs
@@ -173,7 +108,7 @@ class Houzz:
         """
         if filename[0] == '/':
             return filename
-        else: 
+        else:
             # need to append the root folder
             return self.mat_data_folder + '/' + filename
 
--- partition.py
+++ partition.py
@@ -1,36 +1,21 @@
-"""
-Script to partition the dataset into training and test sets.
-"""
+def partition(file_list='bedroom.txt'):
+    """
+    Partitions the dataset into training and test sets.
 
-import os
-import cPickle as pickle
+    @param file_list
+        a text file in the format expected by Caffe.
+        <xxxx.jpg> <int>
+    """
+    TEST_FRACTION = 10  # 1 / TEST_FRACTION for test set
 
-from houzz import DATASET_ROOT
-
-# Get a list of filenames for pickled metadata
-PICKLED_PATH = DATASET_ROOT + 'metadata/bedroom/pkl/'
-pickles = os.listdir(PICKLED_PATH)
-# Two of these files do not actually contain metadata ...
-pickles.remove('successes.pkl')
-pickles.remove('failures.pkl')
-
-"""
-Keep images with text data (description or tag).
-"""
-
-keep = []
-
-for pkl in pickles:
-    with open(PICKLED_PATH + pkl) as fd:
-        data = pickle.load(fd)  # a dictionary 
-        if data['tag'] or data['description']:
-            keep.append(pkl.rstrip('.pkl'))
-            # We'll use this name to find the corresponding image
-
-"""
-Go through the images and text data for the items we have kept
-and load their features.
-
-Precondition: feature representations have been computed for images
-and text.
-"""
+    train = dict()
+    test = dict()
+    with open(file_list) as f:
+        for i, line in enumerate(f):
+            filename = line.split()[0].rstrip('.jpg')
+            label = int(line.split()[1])
+            if i % TEST_FRACTION == 0:
+                test[filename] = label
+            else:
+                train[filename] = label
+    return (train, test)
