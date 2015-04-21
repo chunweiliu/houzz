077a607a50951738a22da1735fa44a90de45404f
Chun-Wei Liu <chunwei@cs.unc.edu>
2015-04-21 10:10:36 -0400

    switched to liblinear in training

--- learning.py
+++ learning.py
@@ -2,9 +2,8 @@
 Subroutines for training the SVM.
 """
 import houzz
-import random
 import numpy
-from svmutil import *
+from liblinearutil import *
 import collections
 
 from features import *
@@ -34,74 +33,23 @@ def grid_search(y, x, k=5):
     """
 
     # Recommended grids from LIBSVM guide
-    c_grid     = [2**i for i in xrange(-5, 15, 2)]
-    gamma_grid = [2**i for i in xrange(-15, 3, 2)]
+    c_grid = [2**i for i in xrange(-5, 15, 2)]
 
     best_accuracy = 0.0
-    for c, gamma in zip(c_grid, gamma_grid):
-        # avg_accuracy = 0.0
-        # for i in xrange(k):
+    for c in c_grid:
 
-        #     format_print("Cross-validating with held-out set {0}, "
-        #                  "c = {1}, gamma = {2}.".format(i, c, gamma))
-
-        #     x_hold = x_split[i]
-        #     y_hold = y_split[i]
-
-        #     x_train = flatten([split for split in x_split if split is not x_hold])
-        #     y_train = flatten([split for split in y_split if split is not y_hold])
-
-        #     # LIBSVM options:
-        #     # -c <cost  parameter>
-        #     # -g <gamma parameter>
-        #     # -q suppress output
-        #     options = "-c {0} -g {1} -q".format(c, gamma)
-        #     model = svm_train(y_train, x_train, options)
-        #     predicted_labels, _, _ = svm_predict(y_hold, x_hold, model)
-
-        #     # compute number of incorrect labels
-        #     num_wrong = 0
-        #     for i, label in enumerate(y_hold):
-        #         if label != predicted_labels[i]:
-        #             num_wrong += 1
-
-        #     avg_accuracy += num_wrong  # keep a running total
-
-        # avg_accuracy /= k  # avg_accuracy is type float
-
-        options = "-c {0} -g {1} -v {2} -q".format(c, gamma, k)
-        accuracy = svm_train(y, x, options)
+        options = "-c {0} -v {1} -q".format(c, k)
+        accuracy = train(y, x, options)
         if accuracy > best_accuracy:
             best_accuracy = accuracy
             c_best = c
-            gamma_best = gamma
 
     format_print(
-    """Cross-validation finished!
-        c = {0}
-        gamma = {1}
-        best accuracy = {2}""".format(c, gamma, best_accuracy)
+        """Cross-validation finished!
+           c = {0}
+           best accuracy = {1}""".format(c, best_accuracy)
     )
-    return c_best, gamma_best
-
-
-def split_data(x, y, k):
-    # k splits
-    x_split = [[] for i in range(k)]
-    y_split = [[] for i in range(k)]
-    # Why not [[]]*k ?
-    # It reuses the same reference for [] each time!
-
-    # Randomly permute indices and count off 0, 1, ..., k-1
-    # assert len(y) == len(x)
-    indices = range(len(y))
-    random.shuffle(indices)
-    for i in xrange(len(y)):
-        fold_number = i % k  # cycles through 0, 1, ..., k-1
-        x_split[fold_number].append(x[indices[i]])
-        y_split[fold_number].append(y[indices[i]])
-
-    return x_split, y_split
+    return c_best
 
 
 def flatten(list_of_lists):
@@ -134,30 +82,28 @@ def train_svm(name, training_labels, img_feature_dir,
     x, y = load_dataset(training_labels, img_feature_dir)
     # x,y = debug(x,y)  # debug
 
-    c, gamma = grid_search(y, x)
+    c = grid_search(y, x)
 
     format_print("Cross validation complete.")
-    format_print("C = {0}, gamma = {1}\n".format(c, gamma))
+    format_print("C = {0}\n".format(c))
 
     # Using the values of 'C' and 'gamma' we got from cross-validation,
     # re-train on the data set.
     # -b 1 -> use probability estimates
     # -q   -> suppress output
     # RBF kernel by default
-
     weights = compute_weights(training_labels)
-    options = "-c {0} -g {1} -b 1 -q {2}".format(c, gamma, weights)
+    options = "-c {0} -q {1}".format(c, weights)
 
-    # options = "-c {0} -g {1} -b 1 -q".format(c, gamma)
     format_print(options)
     format_print("Training SVM with cross-validation parameters ...")
-    model = svm_train(y, x, options)
+    model = train(y, x, options)
     format_print("Training complete.")
 
     # Save model for the future
     model_name = name + '.model' if not name.endswith('.model') else name
     format_print("Saving model to " + houzz.TRAINED_PATH + model_name + " ...")
-    svm_save_model(houzz.TRAINED_PATH + model_name, model)
+    save_model(houzz.TRAINED_PATH + model_name, model)
     format_print("Done.")
 
     return model
@@ -175,23 +121,6 @@ def load_dataset(names_to_labels, img_feature_dir):
         x.append(feature(stem, img_feature_dir))
         y.append(names_to_labels[stem])
     return x, y
-
-
-# def test(model, test_labels, img_feature_dir):
-#     """
-#     @param model: LIBSVM model
-#     @param test_labels (dict: str -> int)
-#     @param img_feature_dir (str)
-#     @return list of predicted labels
-#     @return accuracy
-#     """
-#     x, y = load_dataset(test_labels, img_feature_dir)
-#     x,y = debug(x,y)  # debug
-#     print y.count(0)
-#     print y.count(1)
-
-#     labels, stats, _ = svm_predict(y, x, model, '-q')
-#     return labels, stats[0]  # accuracy
 
 
 def confusion(expected, actual, percentages=True):
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import svmutil
+# import svmutil
+import liblinearutil
 
 from learning import train_svm
 from learning import confusion
@@ -9,19 +10,22 @@ from houzz import partition
 # Training
 model_name = 'caffenet_text_small'
 img_feature_dir = '/home/chunwei/Data/houzz/caffenet_features'
-(train_labels, test_labels) = partition(test_fraction=10, stop=10000)
+(train_labels, test_labels) = partition(test_fraction=10, stop=500)
 train_svm(model_name, train_labels, img_feature_dir)
 
 # Testing
 model_file = '/home/chunwei/Data/houzz/trained_svms/' + model_name + '.model'
-model = svmutil.svm_load_model(model_file)
+# model = svmutil.svm_load_model(model_file)
+model = liblinearutil.load_model(model_file)
 
 x, y = load_dataset(test_labels, img_feature_dir)
 # x, y = debug(x, y)
 # print "DEBUG"
 # print y.count(0)
 # print y.count(1)
-predicted_labels, stats, p_val = svmutil.svm_predict(y, x, model, '-q')
+# predicted_labels, stats, p_val = svmutil.svm_predict(y, x, model, '-q')
+predicted_labels, stats, p_val = liblinearutil.predict(y, x, model, '-q')
+
 accuracy = stats[0]
 
 # Visualize the result
