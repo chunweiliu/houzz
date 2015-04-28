e2885e3122e938509047538bc2fe605bd03c1358
Brian Cristante <brian.cristante@gmail.com>
2015-04-28 16:08:47 -0400

    GIST

---
+++ gist.py
@@ -0,0 +1,25 @@
+import houzz
+import numpy
+import pyleargist
+from format_print import format_print
+from PIL import Image
+
+def generate_features(txt_file, img_dir, output_dir):
+    """
+    Compute and pickle GIST descriptors
+    """
+    img_dir = houzz.standardize(img_dir)
+    output_dir = houzz.standardize(output_dir)
+    with open(txt_file, 'r') as dataset:
+        for line in dataset:
+            img_file = line.split()[0]
+            # Load image and compute GIST
+            im = Image.open(img_dir + img_file)
+            gist = pyleargist.color_gist(im)
+
+            # Save as .npy
+            npy = img_file.replace('.jpg', '.npy')
+            numpy.save(output_dir + npy, gist)
+            format_print("Output written for {}".format(img_file))
+
+
